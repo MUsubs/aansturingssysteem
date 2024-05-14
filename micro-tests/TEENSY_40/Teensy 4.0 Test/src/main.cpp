b034d2428761e24036a16f81788f17db1eea1a6b
Felix <felix.klaassen@student.hu.nl>
2024-05-14 18:03:03 +0200

    teensy testcode voor demo - motors aan dmv touch switch

--- micro-tests/TEENSY_40/Teensy 4.0 Test/src/main.cpp
+++ micro-tests/TEENSY_40/Teensy 4.0 Test/src/main.cpp
@@ -1,18 +1,63 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// declare pin numbers:
+int pin_fwd1 = 21;
+int pin_fwd2 = 20;
+
+int pin_height1 = 19;
+int pin_height2 = 18;
+
+int pin_steer1 = 4;
+int pin_steer2 = 5;
+
+int pin_touch = 8;
+
+void touchTest();
+bool touched;
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  touched = false;
+  // Set up serial and pins:
+  Serial.begin(115200);
+  pinMode( pin_fwd1, OUTPUT );
+  pinMode( pin_fwd2, OUTPUT );
+  pinMode( pin_height1, OUTPUT );
+  pinMode( pin_height2, OUTPUT );
+  pinMode( pin_steer1, OUTPUT );
+  pinMode( pin_steer2, OUTPUT );
+
+  pinMode( pin_touch, INPUT );
+
+  digitalWrite( pin_fwd1, LOW );
+  digitalWrite( pin_fwd2, LOW );
+  digitalWrite( pin_height1, LOW );
+  digitalWrite( pin_height2, LOW );
+  digitalWrite( pin_steer1, LOW );
+  digitalWrite( pin_steer2, LOW );
+
+  // Print start
+  Serial.print( "start" );
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  touchTest();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+
+// Test function for demo.
+void touchTest() {
+//All motors on if touch switch on.
+  if ( digitalRead( pin_touch )) {
+    digitalWrite( pin_steer1, HIGH );
+    digitalWrite( pin_height1, HIGH );
+    digitalWrite( pin_fwd1, HIGH );
+  } else {
+    digitalWrite( pin_steer1, LOW );
+    digitalWrite( pin_height1, LOW );
+    digitalWrite( pin_fwd1, LOW );
+  }
 }
