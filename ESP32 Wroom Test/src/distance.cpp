65bb97fa67943d05e46fdf2ec4f8fa3bd032aeb3
Jorg <jorg.vandenburg@student.hu.nl>
2024-05-29 15:05:15 +0200

    imported test files

---
+++ ESP32 Wroom Test/src/distance.cpp
@@ -0,0 +1,25 @@
+#pragma once
+
+void threadDistance( const uint8_t trig_pin, const uint8_t echo_pin){
+
+  // setup
+  pinMode( trig_pin, OUTPUT ); // Set trigger pin as output
+  pinMode( echo_pin, INPUT );  // Set echo pin as input
+
+  // loop
+  while (1) {
+    // Trigger a sound wave
+    digitalWrite( trig_pin, LOW );
+    delayMicroseconds( 2 );
+    digitalWrite( trig_pin, HIGH );
+    delayMicroseconds( 10 );
+    digitalWrite( trig_pin, LOW );
+
+    // Measure echo time
+    uint32_t duration = pulseIn( echo_pin, HIGH );
+
+    // Calculate distance (speed of sound * time / 2)
+    uint32_t distance = duration * 0.034 / 2; // Adjust factor for speed of sound in your units
+    Serial.println(distance);
+  }
+}
---
+++ ESP32 Wroom Test/src/fib.cpp
@@ -0,0 +1,29 @@
+#pragma once
+
+float fibonacci( int n ) {
+  if ( n <= 1 ) {
+    return n;
+  }
+  return fibonacci( n - 1 ) + fibonacci( n - 2 );
+}
+
+float measureTime( int n ) {
+  unsigned long start = micros();
+  fibonacci( n );
+  unsigned long end = micros();
+  return end - start;
+}
+
+void threadFib( int n ){
+
+  // setup
+  Serial.println( "start fib" );
+
+  // loop
+  while (1) {
+    unsigned long time = measureTime( n );
+    Serial.print( "Fibonacci in ");
+    Serial.print( time );
+    Serial.println( " microseconds." );
+  }
+}
