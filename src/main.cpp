dcc05d0157da6f9fa8ba678f65b02922bb120fca
Jorg <jorg.vandenburg@student.hu.nl>
2024-06-17 13:22:40 +0200

    Added buttons to do forward, backwards, up and down controls
    Co-authored-by: wesselvtoorn <wesselvtoorn@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 #include "mpu6050.hpp"
 #include "motor.hpp"
 
-static uint8_t motor_pins[7] = { 22, 21, 20, 19, 18, 4, 5 };
+static uint8_t motor_pins[7] = { 22, 21, 20, 19, 18, 12, 13 };
+static uint8_t button_pins[4] = { 16, 17, 26, 27 };
 Motor motor( motor_pins );
 MPU6050 mpu( Wire );
 Kalman kalmanFilter;
@@ -17,30 +18,38 @@ void setup(){
     adc_init();
     adc_set_temp_sensor_enabled (true);
     adc_select_input(4);
-    
-    Serial.println("Motor control start");
-    motor.forwardDirection();
-    delay(1000);
-    motor.upDirection();
-    delay(1000);
-    motor.off();
 }
 
 void loop(){
     Serial.print("graden: " + String(round(gyro.getCurrent_z())));
     if(round(gyro.getCurrent_z()) < gyro.getSetpoint()){
         Serial.println(" left ");
-        motor.upDirection();
+        motor.leftDirection();
         delay(50);
     }
     else if(round(gyro.getCurrent_z()) > gyro.getSetpoint()){
         Serial.println(" right ");
-        motor.forwardDirection();
+        motor.rightDirection();
         delay(50);
     }
-    else{
-        Serial.println(" off");
-        motor.off();
+    if (digitalRead(button_pins[0]) == HIGH){
+        motor.forwardDirection();
         delay(50);
+        motor.off();
+    }
+    if (digitalRead(button_pins[1]) == HIGH){
+        motor.backwardDirection();
+        delay(50);
+        motor.off();
+    }
+    if (digitalRead(button_pins[2]) == HIGH){
+        motor.upDirection();
+        delay(50);
+        motor.off();
+    }
+    if (digitalRead(button_pins[3]) == HIGH){
+        motor.downDirection();
+        delay(50);
+        motor.off();
     }
 }
